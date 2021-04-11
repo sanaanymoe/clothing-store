696f9d7bfd5407518e89546f5ad57c1de81bccaa
sanaanymoe <sanaany1992@gmail.com>
2021-04-11 02:29:34 -0700

    add MenuItem and Directory components

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,41 +1,11 @@
 import React from 'react'
 
+import Directory from '../../components/Directory/Directory'
 import './HomePage.styles.scss'
 
 const HomePage = () => (
     <div className='homePage'>
-        <div className='directory-menu'>
-            <div className='menu-item'>
-                <div className='content'>
-                    <h1 className='title'>HATS</h1>
-                    <span className='subtitle'>SHOP NOW</span>
-                </div>
-            </div>
-            <div className='menu-item'>
-                <div className='content'>
-                    <h1 className='title'>JACKETS</h1>
-                    <span className='subtitle'>SHOP NOW</span>
-                </div>
-            </div>
-            <div className='menu-item'>
-                <div className='content'>
-                    <h1 className='title'>SNEAKERS</h1>
-                    <span className='subtitle'>SHOP NOW</span>
-                </div>
-            </div>
-            <div className='menu-item'>
-                <div className='content'>
-                    <h1 className='title'>WOMENS</h1>
-                    <span className='subtitle'>SHOP NOW</span>
-                </div>
-            </div>
-            <div className='menu-item'>
-                <div className='content'>
-                    <h1 className='title'>MENS</h1>
-                    <span className='subtitle'>SHOP NOW</span>
-                </div>
-            </div>
-        </div>
+        <Directory />
     </div>
 )
 
