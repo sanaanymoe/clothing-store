822a08c3c03382353eaabd4a84c79f4cf88db7ea
sanaanymoe <sanaany1992@gmail.com>
2021-04-14 18:19:47 -0700

    add collection item preview and style the items preview

--- src/components/CollectionPreview/CollectionPreview.jsx
+++ src/components/CollectionPreview/CollectionPreview.jsx
@@ -2,15 +2,15 @@ import React from "react";
 
 import "./CollectionPreview.styles.scss";
 
+import CollectionItem from '../CollectionItem/CollectionItem'
+
 const CollectionPreview = ({ title, items }) => {
   return (
     <div className="collection-preview">
       <h1 className="title">{title.toUpperCase()}</h1>
       <div className="preview">
-        {items
-          .slice(0,4)
-          .map((item) => (
-            <div key={item.id}>{item.name}</div>
+        {items.slice(0, 4).map(({ id, ...restProps }) => (
+          <CollectionItem key={id} {...restProps}/>
         ))}
       </div>
     </div>
