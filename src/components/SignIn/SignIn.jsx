c08122c79e716e5d52e6c766ddbd1f0efe462426
sanaanymoe <sanaany1992@gmail.com>
2021-04-30 18:56:47 -0700

    make signin / signup form reusable

--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import FormInput from '../FormInput/FormInput'
 import './SignIn.styles.scss'
 
 class SignIn extends React.Component{
@@ -31,23 +32,23 @@ class SignIn extends React.Component{
             <span>Sign in with your email and password</span>
 
             <form onSubmit={this.handelSubmit}>
-              <input
+              <FormInput
                 name="email"
                 type="email"
                 value={this.state.email}
-                onChange={this.handleChange}
+                handleChange={this.handleChange}
+                label="email"
                 required
               />
-              <label>Email</label>
-              
-              <input
+
+              <FormInput
                 name="password"
                 type="password"
                 value={this.state.password}
-                onChange={this.handleChange}
+                handleChange={this.handleChange}
+                label="password"
                 required
               />
-              <label>Password</label>
 
               <input type="submit" value="Submit Form" />
             </form>
