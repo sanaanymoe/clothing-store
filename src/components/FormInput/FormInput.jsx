c26453ecf84f10d79504fc99decf831004f4c407
sanaanymoe <sanaany1992@gmail.com>
2021-06-22 17:07:09 -0700

    use && for conditions instead of ternary operator

--- src/components/FormInput/FormInput.jsx
+++ src/components/FormInput/FormInput.jsx
@@ -6,11 +6,10 @@ const FormInput = ({ handleChange, label, ...otherProps }) => (
     <div className='group'>
         <input className='form-input' onChange={handleChange} {...otherProps}/>
         {
-            label ? 
+            label &&
             (<label className={`${otherProps.value.length ? 'shrink' : ''} form-input-label`}>
                 {label}
             </label>)
-            : null
         }
     </div>
 )
